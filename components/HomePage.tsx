213c937acc50cbc12c8f56aeb14a0b5d2144259d
code-lova <ebinuga@hotmail.com>
2025-09-18 00:17:52 +0100

    doc: project initialization and environment setup

---
+++ components/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const HomePage = () => {
+  return (
+    <div className="min-h-screen bg-white">
+        <h1 className="text-4xl font-bold text-navy">Welcome to FitShare Life</h1>
+    </div>
+  );
+};
+
+export default HomePage;

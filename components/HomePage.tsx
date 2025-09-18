4e773dd0d8cb674579891b3411d9b69b2342567c
code-lova <ebinuga@hotmail.com>
2025-09-18 14:34:02 +0100

    doc: landing page

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -1,10 +1,24 @@
 import React from "react";
-
+import Header from "./page-components/Header";
+import HeroSection from "./page-components/HeroSection";
+import StatsSection from "./page-components/StatsSection";
+import FeaturesSection from "./page-components/FeaturesSection";
+import WorkoutTypesSection from "./page-components/WorkoutTypesSection";
+import CommunitySection from "./page-components/CommunitySection";
+import CTASection from "./page-components/CTASection";
+import Footer from "./page-components/Footer";
 
 const HomePage = () => {
   return (
-    <div className="min-h-screen bg-white">
-        <h1 className="text-4xl font-bold text-navy">Welcome to FitShare Life</h1>
+    <div className="min-h-screen bg-background">
+      <Header />
+      <HeroSection />
+      <StatsSection />
+      <FeaturesSection />
+      <WorkoutTypesSection />
+      <CommunitySection />
+      <CTASection />
+      <Footer />
     </div>
   );
 };

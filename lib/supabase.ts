997b8782285f96dd62ab86db775c9147a35e469b
code-lova <ebinuga@hotmail.com>
2025-09-23 20:53:26 +0100

    doc:fix run time errors and link

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,15 @@
-import { createClient } from '@supabase/supabase-js';
-import { Database } from '@/types/supabase';
+import { createClient } from "@supabase/supabase-js";
+import { Database } from "@/types/supabase";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-const supabaseAnonKey = process.env
-  .NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY as string;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
+
+if (!supabaseUrl) {
+  throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_URL");
+}
+
+if (!supabaseAnonKey) {
+  throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY");
+}
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);

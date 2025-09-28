c57eb182b12ab22a7058924b0cedf086fa3180c9
Menocas17 <rodolfomenocal7@gmail.com>
2025-09-27 21:53:14 -0600

    finsihing the styling and fixing some bugs, I also changed the table for divs and CSS grid for better control over the layout

--- app/(account)/workout-management/log/[id]/page.tsx
+++ app/(account)/workout-management/log/[id]/page.tsx
@@ -1,6 +1,8 @@
 import TrainingLog from '@/components/workout/log-workout/training-log';
 import { getExercisesByWorkout } from '@/lib/server_actions/workouts';
 import { WorkoutExerciseWithDetails } from '@/types/types';
+import { Dumbbell } from 'lucide-react';
+import { Button } from '@/components/ui/button';
 
 interface PageProps {
   params: {
@@ -17,9 +19,23 @@ export default async function LogPage({ params }: PageProps) {
   console.log(exercises);
 
   return (
-    <>
-      <h2>{exercises[0]?.workouts?.name}</h2>
+    <div className='mx-auto overflow-x-auto w-full'>
+      <div className='flex gap-15'>
+        <div className='flex gap-2 justify-center md:justify-start'>
+          <h2 className='text-2xl sm:text-2xl font-semibold  mb-10'>
+            {exercises[0]?.workouts?.name}
+          </h2>
+          <Dumbbell className='w-8 h-8 text-primary' />
+        </div>
+        <Button
+          variant='outline'
+          className='bg-[#2fb981] hover:bg-[#0a9667] text-white'
+        >
+          Add New
+        </Button>
+      </div>
+
       <TrainingLog exercises={exercises} />
-    </>
+    </div>
   );
 }

88ca7ce52c8177f88c9ce5fe9433f9f79b480377
Menocas17 <rodolfomenocal7@gmail.com>
2025-10-05 11:27:41 -0600

    Created the "my profile page" in which all your personal information is displayed in one place

---
+++ app/(account)/myprofile/page.tsx
@@ -0,0 +1,16 @@
+import Profile from '@/components/social/Profile';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+
+export default async function MyProfilePage() {
+  const cookieStore = await cookies();
+  const sessionToken = cookieStore.get('sessionToken');
+
+  console.log(sessionToken);
+
+  if (!sessionToken) {
+    redirect('/login');
+  }
+
+  return <Profile session={sessionToken.value} />;
+}

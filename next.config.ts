3d7f24a76c4c0f88f0facf3c8d3bd732f4413863
Menocas17 <rodolfomenocal7@gmail.com>
2025-10-05 12:25:56 -0600

    added the image domian to the next.config

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'images.unsplash.com',
+        hostname: 'bnjrdleddwgdcfdpdmpe.supabase.co',
       },
       {
         protocol: 'https',

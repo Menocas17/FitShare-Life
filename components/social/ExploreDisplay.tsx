c2fbe48c7ab79c55aba7e7deabdc9e9e354af89e
Rodolfo Menocal <rodolfomenocal17@gmail.com>
2026-01-19 14:24:03 -0600

    Separated the List of Result in the search of the explore page

--- components/social/ExploreDisplay.tsx
+++ components/social/ExploreDisplay.tsx
@@ -4,10 +4,9 @@ import { useDebouncedCallback } from 'use-debounce';
 import { useState, useTransition } from 'react';
 import LoadingSpinnerSm from '../ui-kit/SmallLoadingSpinner';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link';
-import Image from 'next/image';
+import ResultList from './ResultsList';
 
-type UserSearchResults = {
+export type UserSearchResults = {
   id: string;
   user_id: string;
   user_name: string | null;
@@ -38,7 +37,7 @@ export default function ExploreDisplay({
       });
     },
     // the debounce time
-    300
+    300,
   );
 
   const showNoResults =
@@ -66,45 +65,8 @@ export default function ExploreDisplay({
             <LoadingSpinnerSm text='searching users' />
           </div>
         )}
-        {/* TODO - Separate the list of results to a separete component, and add
-        the links(create the dedicated profile for the results) */}
-        {showResults && (
-          <div className='bg-white shadow-2xl ring-1 ring-black/5 rounded-b-xl py-2 overflow-hidden z-10 absolute w-full '>
-            <div>
-              {userResults.map((result) => {
-                return (
-                  <Link
-                    key={result.id}
-                    href={`explore/user/${result.id}`}
-                    className='flex items-start gap-4  hover:bg-gray-100 p-3 rounded-lg hover:scale-[1.01] transition-all duration-200'
-                  >
-                    {result.users.avatar ? (
-                      <Image
-                        src={result.users.avatar}
-                        alt={`${result.user_name}'s photo`}
-                        width={50}
-                        height={50}
-                        className='w-10 h-10 rounded-full'
-                      />
-                    ) : (
-                      <div className='w-10 h-10 rounded-full bg-gray-400 flex items-center justify-center text-white text-4xl'>
-                        {result.user_name![0]}
-                      </div>
-                    )}
-                    <div>
-                      <p className='text-base leading-tight'>
-                        {result.user_name}
-                      </p>
-                      <p className='text-xs text-muted-foreground leading-tight'>
-                        {result.users.name}
-                      </p>
-                    </div>
-                  </Link>
-                );
-              })}
-            </div>
-          </div>
-        )}
+
+        {showResults && <ResultList userResults={userResults} />}
         {showNoResults && (
           <div className='bg-white shadow-2xl ring-1 ring-black/5 rounded-b-xl py-2 overflow-hidden z-50 absolute w-full flex items-center justify-center min-h-20'>
             <p>No users were found with that user name</p>

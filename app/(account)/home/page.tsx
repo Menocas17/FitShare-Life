c38cb8b2f94ff028490f8e88c8a2ddbdb681887c
Menocas17 <rodolfomenocal7@gmail.com>
2026-02-10 13:04:28 -0600

    I improved the home dashboard and made every component of the dashboar UI completly independent

--- app/(account)/home/page.tsx
+++ app/(account)/home/page.tsx
@@ -1,7 +1,5 @@
-import { getDashboardData } from '@/lib/dashboard';
 import { getUserAndProfileIds } from '@/lib/auth';
-import { parseBodyMeasurements } from '@/lib/utils';
-import { getUserProfile } from '@/lib/profileInfo';
+
 import HomeDashboard from '@/components/home/Dasboard';
 import StatsOverview from '@/components/home/StatsOverview';
 import RecentWorkoutCard from '@/components/home/RecentWorkout';
@@ -11,36 +9,14 @@ import { redirect } from 'next/navigation';
 export default async function Dashboard() {
   const user = await getUserAndProfileIds();
   if (!user) return redirect('/login');
-  const [profile, data] = await Promise.all([
-    getUserProfile(user.userId),
-    getDashboardData(user.profileId),
-  ]);
-
-  if (!data || !profile) {
-    return <div>Error in loading the data or user not found</div>;
-  }
-  const { recentWorkout, dashboardStats, postsCount } = data;
-
-  const bodyMeasurements = parseBodyMeasurements(
-    profile?.body_measurements || null,
-  );
 
   return (
     <HomeDashboard
-      profile={profile}
       user={user}
-      dashboardStats={dashboardStats}
-      stats={
-        <StatsOverview
-          profile={profile}
-          dashboardStats={dashboardStats}
-          postsCount={postsCount}
-        />
-      }
-      postsCount={postsCount}
+      stats={<StatsOverview userId={user.userId} profileId={user.profileId} />}
     >
-      <RecentWorkoutCard recentWorkout={recentWorkout} />
-      <BodyMeasurements bodyMeasurements={bodyMeasurements} />
+      <RecentWorkoutCard profileId={user.profileId} />
+      <BodyMeasurements userId={user.userId} />
     </HomeDashboard>
   );
 }

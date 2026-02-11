9271d1d95390f9d15b9f1921c341cf9134df7a06
Rodolfo Menocal <rodolfomenocal17@gmail.com>
2026-02-11 15:24:47 -0600

    improved spacing in the body measurements and recent workout cards in the home dashboard

--- components/home/BodyMeasurements.tsx
+++ components/home/BodyMeasurements.tsx
@@ -13,7 +13,7 @@ export default async function BodyMeasurements({ userId }: { userId: string }) {
 
   return (
     <div className='p-3 sm:p-4 lg:p-6 bg-card border border-border rounded-lg'>
-      <div className='flex justify-between'>
+      <div className='flex justify-between mb-5'>
         <div className='flex items-center gap-2 mb-4 sm:mb-4'>
           <GanttChart className='w-4 h-4 sm:w-5 sm:h-5 text-primary' />
           <h3 className='text-base sm:text-lg font-semibold'>
